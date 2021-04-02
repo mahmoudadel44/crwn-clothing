04e92bccb1567da9c82ffea4da6ab40c21228684
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2021-04-02 21:54:52 +0200

    adding-home-page and create styles for menu-item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import { HomePage } from "./homepage.component";
+import { HomePage } from "./pages/homepage/homepage.component";
 function App() {
   return (
     <div>
-      Hello World
       <HomePage />
     </div>
   );
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="homepage">
+        <Directory />
+      </div>
+    );
+  }
+}
+
+export { HomePage };
