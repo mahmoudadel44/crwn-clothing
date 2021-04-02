aa6db46368da3e2b2be1c70180a26fdf98fb5afc
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2021-04-02 13:44:39 +0200

    creating-crwnClothing-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { HomePage } from "./homepage.component";
+function App() {
+  return (
+    <div>
+      Hello World
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
